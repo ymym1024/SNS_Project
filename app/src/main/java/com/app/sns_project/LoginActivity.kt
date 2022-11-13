2a72f0a7f1d718faa8ead5f9366e54f206f10144
suho <yousuho1232@naver.com>
2022-11-13 17:17:12 +0900

    [Add] LoginActivity, SignupActivity, Firebase etc...

--- app/src/main/java/com/app/sns_project/LoginActivity.kt
+++ app/src/main/java/com/app/sns_project/LoginActivity.kt
@@ -2,8 +2,13 @@ package com.app.sns_project
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
@@ -15,7 +20,9 @@ class LoginActivity : AppCompatActivity() {
 
         val loginButton = findViewById<Button>(R.id.loginButton)
         loginButton.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            val userEmail = findViewById<EditText>(R.id.editTextEmail).text.toString()
+            val userPassword = findViewById<EditText>(R.id.editTextPassword).text.toString()
+            doLogin(userEmail, userPassword)
         }
 
         val signupButton = findViewById<Button>(R.id.signupButton)
@@ -23,5 +30,19 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, SignupActivity::class.java))
         }
 
+    }
+
+    private fun doLogin(userEmail: String, password: String) {
+        Firebase.auth.signInWithEmailAndPassword(userEmail, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    startActivity(
+                        Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    Log.w("LoginActivity", "signInWithEmail", it.exception)
+                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/app/sns_project/SignupActivity.kt
+++ app/src/main/java/com/app/sns_project/SignupActivity.kt
@@ -2,8 +2,13 @@ package com.app.sns_project
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +16,35 @@ class SignupActivity : AppCompatActivity() {
         setContentView(R.layout.activity_signup)
 
         supportActionBar?.title = "회원가입"
+
+        val signupButton = findViewById<Button>(R.id.signupButton2)
+        signupButton.setOnClickListener {
+            val userName = findViewById<EditText>(R.id.editTextUsername)
+            val userEmail = findViewById<EditText>(R.id.editTextUserEmail).text.toString()
+            val userPassword = findViewById<EditText>(R.id.editTextUserPassword).text.toString()
+            val userPasswordConfirm = findViewById<EditText>(R.id.editTextUserPasswordComfirm).text.toString()
+
+            if(userPassword.equals(userPasswordConfirm)) {
+                doSignup(userEmail, userPassword)
+            }
+            else {
+                Toast.makeText(this, "비밀번호를 다시 확인해 주세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun doSignup(userEmail: String, password: String) {
+        Firebase.auth.createUserWithEmailAndPassword(userEmail, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "회원가입 성공!!", Toast.LENGTH_SHORT).show()
+                    startActivity(
+                        Intent(this, LoginActivity::class.java))
+                    finish()
+                } else {
+                    Log.w("LoginActivity", "signInWithEmail", it.exception)
+                    Toast.makeText(this, "Creation failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
