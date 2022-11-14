03b8ff7a17f701855e1c5b462144b67bcf22fd0a
zjaym1703 <jym971024@gmail.com>
2022-11-15 02:35:51 +0900

    [FEAT] C3- 게시물 조회 로직 구현

--- app/src/main/java/com/app/sns_project/MainFragment.kt
+++ app/src/main/java/com/app/sns_project/MainFragment.kt
@@ -1,59 +1,91 @@
 package com.app.sns_project
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.app.sns_project.DTO.PostDTO
+import com.app.sns_project.databinding.FragmentMainBinding
+import com.bumptech.glide.Glide
+import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentMainBinding
+    private lateinit var firestore: FirebaseFirestore
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var postList : ArrayList<PostDTO> = arrayListOf()
+    private lateinit var mAdapter : RecyclerViewAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        firestore = FirebaseFirestore.getInstance()
+
+        firestore.collection("post").get().addOnSuccessListener { result ->
+            postList.clear()
+            for (item in result) {
+                val post = item.toObject(PostDTO::class.java)
+                postList.add(post)
+            }
+            setAdapter()
+            mAdapter.notifyDataSetChanged()
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun setAdapter(){
+        mAdapter = RecyclerViewAdapter(postList)
+        binding.postRecyclerview.adapter = mAdapter
+        binding.postRecyclerview.layoutManager = LinearLayoutManager(context)
+        binding.postRecyclerview.setHasFixedSize(true)
+    }
+
+    inner class RecyclerViewAdapter(val itemList: ArrayList<PostDTO>) : RecyclerView.Adapter<RecyclerViewAdapter.CustomViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+            return CustomViewHolder(LayoutInflater.from(context).inflate(R.layout.layout_post_item, parent, false))
+        }
+
+        inner class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val postUser : TextView = itemView.findViewById(R.id.post_user)
+            val userName : TextView = itemView.findViewById(R.id.user_name)
+            val postContent : TextView = itemView.findViewById(R.id.post_content)
+            val postTime : TextView = itemView.findViewById(R.id.post_time)
+            val postImage : ImageView = itemView.findViewById(R.id.post_image)
+            val postFavoriteCnt : TextView = itemView.findViewById(R.id.post_favorite_cnt)
+        }
+
+        override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+            holder.postUser.text = itemList[position].userId
+            holder.userName.text = itemList[position].userId
+            holder.postContent.text = itemList[position].content
+           // holder.postTime.text = convertTimestampToDate(itemList[position].timestamp)
+            if(itemList[position].imageUrl?.isNotEmpty() == true){
+                Glide.with(context!!).load(itemList[position].imageUrl?.get(0)).into(holder.postImage) //첫번째 이미지만 불러오기(임시)
             }
+            if(itemList[position].favoriteCount>0){
+                holder.postFavoriteCnt.text = "${itemList[position].favoriteCount}명이 좋아합니다."
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return postList.size
+        }
+
+        private fun convertTimestampToDate(time: Long?): String {
+            val sdf = SimpleDateFormat("yyyy.MM.dd HH:mm")
+            val date = sdf.format(time).toString()
+            return date
+        }
     }
 }
